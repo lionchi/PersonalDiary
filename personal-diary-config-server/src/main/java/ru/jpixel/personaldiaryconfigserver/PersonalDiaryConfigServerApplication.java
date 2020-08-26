f06c1b9a3187ddff67c97da1522405f855361dfa
Ivan Gavrilov <ivan.gavrilov@fors.ru>
2020-08-26 20:45:44 +0300

    config server

---
+++ personal-diary-config-server/src/main/java/ru/jpixel/personaldiaryconfigserver/PersonalDiaryConfigServerApplication.java
@@ -0,0 +1,15 @@
+package ru.jpixel.personaldiaryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class PersonalDiaryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDiaryConfigServerApplication.class, args);
+	}
+
+}
