09229e4f23cd7b78b33578f9c72e3a946c333f65
Ivan Gavrilov <ivan.gavrilov@fors.ru>
2020-08-27 16:07:36 +0300

    настройка для статических ресурсов

---
+++ personal-diary-client/src/main/java/ru/jpixel/personaldiaryclient/web/configs/SecurityConfig.java
@@ -0,0 +1,29 @@
+package ru.jpixel.personaldiaryclient.web.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/registration").permitAll()
+        .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring()
+                .antMatchers(HttpMethod.GET, "/*.css", "/*.png", "/*.ico", "/*.js", "/*.json")
+                .antMatchers(HttpMethod.GET, "/index.html", "/static/**");
+    }
+}
