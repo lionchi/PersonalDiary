31ecb88094e2d4354d4d6a0ce0e21202617cf98a
Ivan Gavrilov <ivan.gavrilov@fors.ru>
2020-10-20 09:27:48 +0300

    kubernetes

--- personal-diary-client/src/main/java/ru/jpixel/personaldiaryclient/PersonalDiaryClientApplication.java
+++ personal-diary-client/src/main/java/ru/jpixel/personaldiaryclient/PersonalDiaryClientApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class PersonalDiaryClientApplication extends SpringBootServletInitializer {
 
--- personal-diary-mail-service/src/main/java/ru/jpixel/personaldiarymailservice/PersonalDiaryMailServiceApplication.java
+++ personal-diary-mail-service/src/main/java/ru/jpixel/personaldiarymailservice/PersonalDiaryMailServiceApplication.java
@@ -3,14 +3,14 @@ package ru.jpixel.personaldiarymailservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 @EnableAsync
