6052c7e5db969824a99032b62fcb63e92ea1d8bd
Ivan Gavrilov <ivan.gavrilov@fors.ru>
2020-10-21 17:59:12 +0300

    Избавление от ненужных файлов конфигурации

--- personal-diary-user-service/src/main/java/ru/jpixel/personaldiaryuserservice/PersonalDiaryUserServiceApplication.java
+++ personal-diary-user-service/src/main/java/ru/jpixel/personaldiaryuserservice/PersonalDiaryUserServiceApplication.java
@@ -2,10 +2,10 @@ package ru.jpixel.personaldiaryuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class PersonalDiaryUserServiceApplication {
 
 	public static void main(String[] args) {
